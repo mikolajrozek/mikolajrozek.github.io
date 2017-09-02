1202e98e6041bc82d3625c02fe8e229083c4b268
Mikolaj Rozek <mikolajrozek@users.noreply.github.com>
2017-09-02 17:00:08 +0200

    portfolio

---
+++ portfolio/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.cv-info').click(function(){
+        window.open("https://drive.google.com/file/d/0B66GWTKUXa9ESkVyNjliUTJJZ3M/view?usp=sharing",'_blank');
+    });
+    
+    $('.project1').click(function(){
+        window.open("https://mikolajrozek.github.io/projekt_ambitny/index.html",'_blank');
+    });
+    
+    $('.project2').click(function(){
+        window.open("https://mikolajrozek.github.io/projekt_ambitny_2/index.html",'_blank');
+    });
+    
+    $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 700, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
