faa44370e481fbb3e602a18f78e12d80ebfe9572
Mikolaj Rozek <mikolajrozek@users.noreply.github.com>
2017-09-03 16:33:44 +0200

    bootstrap layout

---
+++ bootstrap/assets/js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+	var $window = $(window);
+	// Tutorial: http://code.tutsplus.com/tutorials/a-simple-parallax-scrolling-technique--net-27641
+	$('section[data-type="background"]').each(function(){
+		var $bgobj = $(this);
+		
+		$(window).scroll(function() {
+			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+			var coords = '50% '+ yPos + 'px';
+			$bgobj.css({ backgroundPosition: coords });
+		});
+	});
+	
+});
