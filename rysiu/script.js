e26783e739e7a2eb9f592f373cf68edb3fc67376
Mikolaj Rozek <mikolajrozek@users.noreply.github.com>
2017-11-21 23:25:42 +0100

    "projekt ver. 1.1"

--- rysiu/script.js
+++ rysiu/script.js
@@ -17,3 +17,21 @@ $(function() {
           });
      });
 });
+
+$('.navbar').hide();
+$('.navbar').fadeIn(900);
+$('.jumbotron').hide();
+$('.jumbotron').fadeIn(2500);
+
+$(function() {
+    $(window).scroll(function() {
+        $('.hideme').each(function(i) {
+            var bottom_of_obj = $(this).position().top + $(this).outerHeight();
+            var bottom_of_win = $(window).scrollTop() + $(window).height();
+
+            if(bottom_of_win > bottom_of_obj) {
+                $(this).animate({'opacity':'1'}, 1500);
+            }
+        });
+    });
+});
