831ea08eeed2a49ca4d6210537af5422af587ac2
Mikolaj Rozek <mikolajrozek@users.noreply.github.com>
2018-02-12 17:33:59 +0100

    cv_portfolio

--- cv_portfolio/js/script.js
+++ cv_portfolio/js/script.js
@@ -1,49 +1,20 @@
 $(document).ready(function() {
 
-	$('h2 #link1').mouseenter(function() {
-		$('#link1').css({
-			fontSize: '1.05em',
+	$('.link').mouseenter(function() {
+		$(this).css({
+			fontSize: '1.2em',
 			transition: '0.6s'
 		});
 	}).mouseleave(function() {
-		$('#link1').css({
+		$(this).css({
 			fontSize: '1.0em'
 		});
 	});
 
-	$('h2 #link2').mouseenter(function() {
-		$('#link2').css({
-			fontSize: '1.05em',
-			transition: '0.6s'
-		});
+	$('[class*="project"]').mouseenter(function() {
+		$(this).css({transform: 'scale(0.9)', opacity: '1.0'});
 	}).mouseleave(function() {
-		$('#link2').css({
-			fontSize: '1.0em'
-		});
-	});
-
-	$('.project1').mouseenter(function() {
-		$('.project1').css({transform: 'scale(0.9)', opacity: '1.0'});
-	}).mouseleave(function() {
-		$('.project1').css({transform: 'scale(0.89)', opacity: '0.7'});
-	});
-
-	$('.project2').mouseenter(function() {
-		$('.project2').css({transform: 'scale(0.9)', opacity: '1.0'});
-	}).mouseleave(function() {
-		$('.project2').css({transform: 'scale(0.89)', opacity: '0.7'});
-	});
-
-	$('.project3').mouseenter(function() {
-		$('.project3').css({transform: 'scale(0.9)', opacity: '1.0'});
-	}).mouseleave(function() {
-		$('.project3').css({transform: 'scale(0.89)', opacity: '0.7'});
-	});
-
-	$('.project4').mouseenter(function() {
-		$('.project4').css({transform: 'scale(0.9)', opacity: '1.0'});
-	}).mouseleave(function() {
-		$('.project4').css({transform: 'scale(0.89)', opacity: '0.7'});
+		$(this).css({transform: 'scale(0.89)', opacity: '0.7'});
 	});
 
 	$('.project1').click(function(){
@@ -61,4 +32,5 @@ $(document).ready(function() {
 	$('.project4').click(function(){
 	    window.open("https://jsfiddle.net/kezoore/ud9rgmaf/",'_blank');
 	});
+
 });
